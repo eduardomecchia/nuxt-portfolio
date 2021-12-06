a72c466b1a0b3459ee0975ba0711ffa6809d57e4
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-12-07 00:53:55 +0100

    Add certificates page

---
+++ pages/certificates/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="certificates">
+    <div class="container">
+      <h1>Certificates</h1>
+      <p>This is the certificates page</p>
+    </div>
+  </main>
+</template>
+
+<script>
+</script>
