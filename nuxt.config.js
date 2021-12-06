ede0735f87be030cbad0baf5803e87b42614750e
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-12-07 00:56:50 +0100

    Change app title to Eduardo Mecchia

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-portfolio',
+    title: 'Eduardo Mecchia',
     htmlAttrs: {
       lang: 'en'
     },
