8d97abeef0ae4e67689f52bd14dd12c10ef1f8af
Eduardo Mecchia <eduardomecchia273@gmail.com>
2022-03-28 15:37:45 +0200

    Style prototype of about section and tweak padding of main section

--- pages/index.vue
+++ pages/index.vue
@@ -42,10 +42,18 @@
 
       <section class="home__section" id="about">
         <h2 class="home__section__heading">About me</h2>
-        <p class="home__paragraph" id="first-paragraph">I’m a 21-year-old software developer from Rome with a strong passion for everything that has to do with technology and innovation.
-        I started tinkering with my first PC when I was 7 years old and since that day I have never stopped: I fell in love with the Internet and its being a free and enormous source of knowledge.
-        That’s why I decided to dive into web development, dreaming to make people’s lives easier thanks to sophisticated and user-friendly products. I've also chosen this field because of its constant evolution and because of my perpetual desire to grow and learn.
-        I’m also a big fan of plain and simple problem solving: one of the greatest sources of satisfaction in my life is fixing the problems of those who don’t know their way around technology and most importantly teach them, if possible, to handle similar barriers themselves in the future.</p>
+
+        <div class="home__section__content">
+          <p class="home__section__content__paragraph" id="first-paragraph">I’m a 21-year-old software developer from Rome with a strong passion for everything that has to do with technology and innovation.
+          I started tinkering with my first PC when I was 7 years old and since that day I have never stopped: I fell in love with the Internet and its being a free and enormous source of knowledge.
+          That’s why I decided to dive into web development, dreaming to make people’s lives easier thanks to sophisticated and user-friendly products. I've also chosen this field because of its constant evolution and because of my perpetual desire to grow and learn.
+          I’m also a big fan of plain and simple problem solving: one of the greatest sources of satisfaction in my life is fixing the problems of those who don’t know their way around technology and most importantly teach them, if possible, to handle similar barriers themselves in the future.</p>
+
+          <!-- Photo of me -->
+          <figure class="home__section__content__figure">
+            <img src="~/assets/images/person.png" alt="Photo of me">
+          </figure>
+        </div>
       </section>
     </main>
 
