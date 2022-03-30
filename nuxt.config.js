175356d9264a44ad41686e58c75dd75863c1b94d
Eduardo Mecchia <eduardomecchia273@gmail.com>
2022-03-30 02:05:31 +0200

    Add 'Web Developer' to app title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Eduardo Mecchia',
+    title: 'Eduardo Mecchia: Web Developer',
     htmlAttrs: {
       lang: 'en'
     },
