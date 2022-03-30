3ab70ced267c4060f6a42b84e66e848734127fba
Eduardo Mecchia <eduardomecchia273@gmail.com>
2022-03-30 18:51:30 +0200

    Transform dividers under section headings in a component

---
+++ components/divider.vue
@@ -0,0 +1,21 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        height: 12px;
+        width: 180px;
+        background: rgb(2,0,36);
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 0%, rgba(6,68,160,1) 12%, rgba(0,212,255,1) 100%);
+        border-radius: 10px;
+        margin: 0 auto;
+        margin-top: 10px;
+    }
+</style>
