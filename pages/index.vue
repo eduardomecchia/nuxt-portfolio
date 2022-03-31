9dd2d5fb29dcf8ffd0d232250efd472e5e5b04d8
Eduardo Mecchia <eduardomecchia273@gmail.com>
2022-04-01 00:36:22 +0200

    Change indentation of index.vue to 4 spaces instead of 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,90 +1,119 @@
 <template>
-  <!-- App wrapper -->
-  <div class="app-wrapper">
-    <!-- Navbar 
-    <header>
-      <nav id="website-navbar">
-        <ul>
-          <li class="navbar-entry">
-            <a href="#" class="navbar-entry_">Eduardo Mecchia</a>
-          </li>
+    <!-- App wrapper -->
+    <div class="app-wrapper">
+        <!-- Navbar 
+        <header>
+            <nav id="website-navbar">
+                <ul>
+                <li class="navbar-entry">
+                    <a href="#" class="navbar-entry_">Eduardo Mecchia</a>
+                </li>
 
-          <li class="navbar-entry">
-            <a href="#" class="navbar-entry_">Projects</a>
-          </li>
-          
-          <li class="navbar-entry">
-            <a href="#" class="navbar-entry_">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header> -->
+                <li class="navbar-entry">
+                    <a href="#" class="navbar-entry_">Projects</a>
+                </li>
+                
+                <li class="navbar-entry">
+                    <a href="#" class="navbar-entry_">Contact</a>
+                </li>
+                </ul>
+            </nav>
+        </header> -->
 
-    <!-- Jumbotron -->
-    <!-- <section id="website-jumbotron">
-      <div class="jumbotron">
-        <div class="jumbotron-content">
-          <h1 class="jumbotron-content_">Eduardo Mecchia</h1>
-          <p class="jumbotron-content_">
-            I'm a software developer, currently working at <a href="https://www.linkedin.com/in/eduardo-mecchia-a0a9b7b4/" target="_blank">LinkedIn</a>.
-          </p>
-        </div>
-      </div>
-    </section> -->
+        <!-- Jumbotron -->
+        <!-- <section id="website-jumbotron">
+            <div class="jumbotron">
+                <div class="jumbotron-content">
+                    <h1 class="jumbotron-content_">Eduardo Mecchia</h1>
+                    <p class="jumbotron-content_">
+                        I'm a software developer, currently working at <a href="https://www.linkedin.com/in/eduardo-mecchia-a0a9b7b4/" target="_blank">LinkedIn</a>.
+                    </p>
+                </div>
+            </div>
+        </section> -->
 
-    <!-- Content -->
-    <main class="home">
-      <div class="home__hero-section">
-        <h1 class="home__hero-section__heading">Hey! I'm Eduardo, a <strong class="gradient-text">full stack web developer</strong>.<br>The website is a WiP, I will be adding my projects and more sections soon!</h1>
-        <b-button @click="scrollToAboutSection()" class="home__cta mail-button scale-on-hover" pill variant="primary">Learn more</b-button>
-      </div>
+        <!-- Content -->
+        <main class="home">
+            <div class="home__hero-section">
+                <h1 class="home__hero-section__heading">
+                Hey! I'm Eduardo, a
+                <strong class="gradient-text">full stack web developer</strong>.<br/>
+                The website is a WiP, I will be adding my projects and more sections soon!</h1>
 
-      <section class="home__section" id="about" ref="aboutSection">
-        <h2 class="home__section__heading">About me</h2>
-        <Divider />
+                <b-button
+                    @click="scrollToAboutSection(this)"
+                    class="home__cta mail-button scale-on-hover"
+                    pill
+                    variant="primary"
+                >Learn more</b-button>
+            </div>
 
-        <div class="home__section__content about__content">
-          <p class="home__section__content__paragraph" id="first-paragraph">I’m a 21-year-old <strong>software developer</strong> from Rome with a strong passion for everything that has to do with technology and innovation.
-          I started tinkering with my first PC when I was 7 years old and since that day I have never stopped: I fell in love with the Internet and its being a free and enormous source of knowledge.
-          That’s why I decided to dive into <strong>web development</strong>, dreaming to make people’s lives easier thanks to sophisticated and <strong>user-friendly</strong> products. I've also chosen this field because of its constant evolution and because of my perpetual desire to grow and learn.
-          I’m also a big fan of plain and simple <strong>problem solving</strong>: one of the greatest sources of satisfaction in my life is fixing the problems of those who don’t know their way around technology and most importantly teach them, if possible, to handle similar barriers themselves in the future.</p>
+            <section class="home__section" id="about" ref="aboutSection">
+                <h2 class="home__section__heading">About me</h2>
+                <Divider />
 
-          <!-- Photo of me -->
-          <figure class="home__section__content__figure">
-            <b-img src="~/assets/images/person.png" fluid alt="Photo of me"></b-img>
-          </figure>
-        </div>
-      </section>
+                <div class="home__section__content about__content">
+                    <p class="home__section__content__paragraph" id="first-paragraph">
+                        I’m a 21-year-old <strong>software developer</strong> from Rome with
+                        a strong passion for everything that has to do with technology and
+                        innovation. I started tinkering with my first PC when I was 7 years
+                        old and since that day I have never stopped: I fell in love with the
+                        Internet and its being a free and enormous source of knowledge.
+                        That’s why I decided to dive into <strong>web development</strong>,
+                        dreaming to make people’s lives easier thanks to sophisticated and
+                        <strong>user-friendly</strong> products. I've also chosen this field
+                        because of its constant evolution and because of my perpetual desire
+                        to grow and learn. I’m also a big fan of plain and simple
+                        <strong>problem solving</strong>: one of the greatest sources of
+                        satisfaction in my life is fixing the problems of those who don’t
+                        know their way around technology and most importantly teach them, if
+                        possible, to handle similar barriers themselves in the future.
+                    </p>
 
-      <section class="home__section" id="contact">
-        <h2 class="home__section__heading">Contact me</h2>
-        <Divider />
+                    <!-- Photo of me -->
+                    <figure class="home__section__content__figure">
+                        <b-img
+                        src="~/assets/images/person.png"
+                        fluid
+                        alt="Photo of me"
+                        ></b-img>
+                    </figure>
+                </div>
+            </section>
 
-        <div class="home__section__content contact__content">
-          <p>Feel free to send me an e-mail regarding everything from business enquiries to feedbacks about my portfolio site.</p>
-          <b-button class="mail-button scale-on-hover" pill variant="primary" href="mailto:eduardo.mecchia.dev@gmail.com">Send Mail</b-button>
-        </div>
-      </section>
-    </main>
+            <section class="home__section" id="contact">
+                <h2 class="home__section__heading">Contact me</h2>
+                <Divider />
 
-    <FollowMe />
+                <div class="home__section__content contact__content">
+                    <p>
+                        Feel free to send me an e-mail regarding everything from business
+                        enquiries to feedbacks about my portfolio site.
+                    </p>
+                    <b-button
+                        class="mail-button scale-on-hover"
+                        pill
+                        variant="primary"
+                        href="mailto:eduardo.mecchia.dev@gmail.com"
+                        >Send Mail</b-button
+                    >
+                </div>
+            </section>
+        </main>
 
-    <footer id="website-footer">&copy;2022 Eduardo Mecchia</footer>
-  </div>
+        <FollowMe />
+
+        <footer id="website-footer">&copy;2022 Eduardo Mecchia</footer>
+    </div>
 </template>
 
 <script>
-  import followMe from '../components/follow-me.vue';
-
-  export default {
-    components: {
-      followMe
+export default {
+  methods: {
+    scrollToAboutSection(object) {
+      console.log(object);
+      this.$refs.aboutSection.scrollIntoView({ behavior: "smooth" });
     },
-
-    methods: {
-      scrollToAboutSection() {
-        this.$refs.aboutSection.scrollIntoView({behavior: 'smooth'});
-      }
-    }
-  }
+  },
+};
 </script>
