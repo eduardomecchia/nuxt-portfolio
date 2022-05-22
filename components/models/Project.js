6dc2bd3b7f88d35c74a0e3cf156dd167da967699
Eduardo Mecchia <eduardomecchia273@gmail.com>
2022-05-22 15:58:52 +0200

    Add Project class model

---
+++ components/models/Project.js
@@ -0,0 +1,10 @@
+export default class Project {
+    constructor(name, description, image, url, technologies, github) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.url = url;
+        this.technologies = technologies;
+        this.github = github;
+    }
+}
