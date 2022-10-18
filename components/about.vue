4d7ca582d2b6f7bdf5dd5a548ed63d6852589eab
Eduardo Mecchia <eduardomecchia273@gmail.com>
2022-10-19 01:54:59 +0200

    feat: add break tags to descriptiojn

--- components/about.vue
+++ components/about.vue
@@ -6,14 +6,19 @@
             <p>
                 I’m a 21-year-old <strong>software developer</strong> from Rome with
                 a strong passion for everything that has to do with technology and
-                innovation. I started tinkering with my first PC when I was 7 years
+                innovation.
+                <br>
+                I started tinkering with my first PC when I was 7 years
                 old and since that day I have never stopped: I fell in love with the
                 Internet and its being a free and enormous source of knowledge.
+                <br>
                 That’s why I decided to dive into <strong>web development</strong>,
                 dreaming to make people’s lives easier thanks to sophisticated and
                 <strong>user-friendly</strong> products. I've also chosen this field
                 because of its constant evolution and because of my perpetual desire
-                to grow and learn. I’m also a big fan of plain and simple
+                to grow and learn.
+                <br>
+                I’m a big fan of plain and simple
                 <strong>problem solving</strong>: one of the greatest sources of
                 satisfaction in my life is fixing the problems of those who don’t
                 know their way around technology and most importantly teach them, if
