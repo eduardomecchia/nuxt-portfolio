c8d0ffdcc1815a55e76d99d2c07e9ccb086615e9
Eduardo Mecchia <eduardomecchia273@gmail.com>
2022-10-28 00:28:52 +0200

    feat: add scale-on-hover class to project cards

--- components/project-card.vue
+++ components/project-card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="project-card shadow p-3 mb-5 rounded">
+    <div class="project-card shadow p-3 mb-5 rounded scale-on-hover">
         <a :href="project.url" target="_blank">
             <b-img 
                 fluid 
